374d639f324312dd17d8ed38ac22d77bf62b8c5f
RobinNS <annoyancehannibal@gmail.com>
2020-08-02 15:33:48 +0700

    masih banyak yg lupo

---
+++ day9.js
@@ -0,0 +1,27 @@
+boo=Boolean
+
+
+
+
+const robin=(boo)=>{
+    if(boo){
+        
+    }else{
+        return  {
+            max:[1,2,(boo)=>{
+                if(boo){
+                 }return {
+                    bayu :{
+                        brian:(boo)=>{
+                            if(boo){
+                                return 'hana'
+                            }
+                        }  
+                    }
+                }   
+            }]
+        }
+    }
+}
+                
+console.log(robin(false).max[2](true)['bayu'].brian(true))
